57c73d62135f7387fc0babec12143f0ad4672299
Leandro Hosken <lshv04@gmail.com>
2024-12-04 14:51:04 -0300

    acrescentado favcon e alterado padrão themeContext para Dark

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -16,7 +16,7 @@ export const useTheme = () => {
 };
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(true);
 
   const toggleTheme = () => {
     setIsDark((prev) => !prev);
